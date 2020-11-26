33b33cd74de1a99aec3e00ff55703f9ca19d8926
Irina Rogova <buyersbirds@gmail.com>
2020-11-26 16:37:25 +0600

    added manager

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Post {
+    private int domain;
+    private int ownerId;
+    private int offset;
+    private String query;
+    private int postId;
+
+}
---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] searchWall(String domain, int ownerId, String query, int offset) {
+        // TODO: add logic
+        return null;
+    }
+
+    public void deleteWall(int ownerId, int postId) {
+        // TODO: add logic
+    }
+}

4cd752fd7b30b6e7d755efd94920b2f2b03e9826
Irina Rogova <buyersbirds@gmail.com>
2020-11-30 11:30:31 +0600

    adedd all classes

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -7,5 +7,14 @@ public class Post {
     private String query;
     private int postId;
     private PostLike postLike;
+    private FormOwner formOwner;
+    private PageOwner pageOwner;
+    private PostComment postComment;
+    private PostCommentSort postCommentSort;
+    private PostComplain postComplain;
+    private PostContent postContent;
+    private PostGeo postGeo;
+    private PostHeader postHeader;
+    private PostSort postSort;
 
 }
